f22fec216f31f342279d650bd128907b354c12f3
Meng <phanducmanhvt2004@gmail.com>
2024-12-30 00:29:47 +0700

    chinh lai frontend

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,15 +1,15 @@
-import {Link} from "react-router-dom";
-import {useState} from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios";
 
 export default function RegisterPage() {
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   async function registerUser(ev) {
     ev.preventDefault();
     try {
-      await axios.post('/register', {
+      await axios.post('v1/auth/register', {
         name,
         email,
         password,
@@ -25,17 +25,17 @@ export default function RegisterPage() {
         <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto" onSubmit={registerUser}>
           <input type="text"
-                 placeholder="John Doe"
-                 value={name}
-                 onChange={ev => setName(ev.target.value)} />
+            placeholder="John Doe"
+            value={name}
+            onChange={ev => setName(ev.target.value)} />
           <input type="email"
-                 placeholder="your@email.com"
-                 value={email}
-                 onChange={ev => setEmail(ev.target.value)} />
+            placeholder="your@email.com"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)} />
           <input type="password"
-                 placeholder="password"
-                 value={password}
-                 onChange={ev => setPassword(ev.target.value)} />
+            placeholder="password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)} />
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
