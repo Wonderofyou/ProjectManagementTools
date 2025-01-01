630e8a7ae9a3541d06edc637d537f643d6f68110
hiww-hu-20 <222120105@student.hcmus.edu.vn>
2025-01-01 14:13:39 +0700

    config header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/account" element={<ProfilePage />} />
         </Route>
+
       </Routes>
     </UserContextProvider>
   )
